7199accd17f2738292e994a737aa44b0b9c2ee5e
Jitin Sardana <jitinsardana@gmail.com>
2022-05-19 13:27:14 +0530

    Added Loading

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <Container>
       <Grid >
-        <Loading loading={isLoading} />
+        {isLoading && <Loading loading={isLoading} /> }
         <TopBar />
         <Routes>
           <Route path="/" element={<Cats />} />
